b20005a6fb95326f9adabb55dda715448e6120ad
rev bar <revital.jer@gmail.com>
2020-11-23 23:02:32 +0200

    page-Row1- final layout

--- js.js
+++ js.js
@@ -1,12 +1,26 @@
-function changePicOn(){
-document.getElementById("link_img").src="images/read_more_blue.png";
-
-
+function changePicOn1(){
+document.getElementById("link_img1").src="images/read_more_blue.png";
 }
 
 
-function changePicOff(){
-    document.getElementById("link_img").src="images/read_more_white.png";
-    
-    
+function changePicOff1(){
+    document.getElementById("link_img1").src="images/read_more_white.png";  
+}
+
+function changePicOn2(){
+    document.getElementById("link_img2").src="images/read_more_blue.png";
     }
+    
+    
+    function changePicOff2(){
+        document.getElementById("link_img2").src="images/read_more_white.png";  
+    }
+
+    function changePicOn3(){
+        document.getElementById("link_img3").src="images/read_more_blue.png";
+        }
+        
+        
+        function changePicOff3(){
+            document.getElementById("link_img3").src="images/read_more_white.png";  
+        }
