2f126d746281689bffe5ef9fa15f0b2a9fa01245
rev bar <revital.jer@gmail.com>
2020-11-23 22:26:11 +0200

    text on image- change color to blue (text+pic)

---
+++ js.js
@@ -0,0 +1,12 @@
+function changePicOn(){
+document.getElementById("link_img").src="images/read_more_blue.png";
+
+
+}
+
+
+function changePicOff(){
+    document.getElementById("link_img").src="images/read_more_white.png";
+    
+    
+    }
