e9477e2cebbf78c1645ff538389153e759ae1b69
rev bar <revital.jer@gmail.com>
2020-11-24 18:31:58 +0200

    page- Row2C3 final layout+ footer links

--- js.js
+++ js.js
@@ -9,36 +9,64 @@ function changePicOff1(){
 
 function changePicOn2(){
     document.getElementById("link_img2").src="images/read_more_blue.png";
-    }
+}
     
     
-    function changePicOff2(){
-        document.getElementById("link_img2").src="images/read_more_white.png";  
-    }
+function changePicOff2(){
+    document.getElementById("link_img2").src="images/read_more_white.png";  
+}
 
-    function changePicOn3(){
-        document.getElementById("link_img3").src="images/read_more_blue.png";
-        }
+function changePicOn3(){
+    document.getElementById("link_img3").src="images/read_more_blue.png";
+}
         
         
-        function changePicOff3(){
-            document.getElementById("link_img3").src="images/read_more_white.png";  
-        }
+function changePicOff3(){
+    document.getElementById("link_img3").src="images/read_more_white.png";  
+}
 
-        function changePicOn4(){
-            document.getElementById("link_img4").src="images/read_more_blue.png";
-            }
+function changePicOn4(){
+    document.getElementById("link_img4").src="images/read_more_blue.png";
+}
             
             
-            function changePicOff4(){
-                document.getElementById("link_img4").src="images/read_more_yellow.png";  
-            }
+function changePicOff4(){
+    document.getElementById("link_img4").src="images/read_more_yellow.png";  
+}
 
-            function changePicOn5(){
-                document.getElementById("link_img5").src="images/read_more_blue.png";
-                }
+function changePicOn5(){
+    document.getElementById("link_img5").src="images/read_more_blue.png";
+}
                 
                 
-                function changePicOff5(){
-                    document.getElementById("link_img5").src="images/read_more_yellow.png";  
-                }
+ function changePicOff5(){
+     document.getElementById("link_img5").src="images/read_more_yellow.png";  
+ }
+
+ function changePicOn6(){
+    document.getElementById("link_img6").src="images/read_more_blue.png";
+}
+                
+                
+ function changePicOff6(){
+     document.getElementById("link_img6").src="images/read_more_yellow.png";  
+ }
+
+ function changePicOn7(){
+    document.getElementById("link_img7").src="images/read_more_blue.png";
+}
+                
+                
+ function changePicOff7(){
+     document.getElementById("link_img7").src="images/read_more_yellow.png";  
+ }
+
+ function changePicOn8(){
+    document.getElementById("link_img8").src="images/read_more_blue.png";
+}
+                
+                
+ function changePicOff8(){
+     document.getElementById("link_img8").src="images/read_more_yellow.png";  
+ }
+
